1cc7268564cf5839e835e88600afb8a9f9f1e02c
Wayder Loyola <wayderloyola@gmail.com>
2021-09-20 12:08:19 -0300

    :tada: Challenge 02 - completed

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
+import { Button } from "./Button";
+import '../styles/sidebar.scss';
+
+type SideBarProps = {
+  genres: GenreResponseProps[];
+  changeGenre: (genre: number) => void;
+  selectedGenre: GenreResponseProps;
+}
+
+type GenreResponseProps = {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar(props: SideBarProps) {
   // Complete aqui
+  function handleClickButton(id: number) {
+    props.changeGenre(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {props.genres.map((genre: GenreResponseProps) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={props.selectedGenre.id === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
